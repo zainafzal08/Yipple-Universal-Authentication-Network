258878bcbd8529e30a7d23ba07600c05da9f440a
Zain Afzal <zain.afz@gmail.com>
2018-04-12 23:08:01 +1000

    Added logging functon

--- assignment0/flaskr/basic/views.py
+++ assignment0/flaskr/basic/views.py
@@ -1,7 +1,26 @@
 from flask import render_template_string, request, render_template
 import re
 from . import app
+import datetime
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+@app.route('/log')
+def log():
+    f = open("log.txt","r")
+    daLog = f.read().split("\n")
+    daLog = [x.split("$") for x in daLog]
+    f.close()
+    return render_template("log.html",log=daLog)
+
+@app.route('/logme')
+def logme():
+    f = open("log.txt","w+")
+    leNow = datetime.datetime.now().strftime("%d %b %Y %I:%M %p")
+    leArgs = ["%s:%s"%(x,request.args[x]) for x in request.args.keys()]
+    leArgs = ",".join(leArgs)
+    f.write("%s$%s"%(leNow,leArgs))
+    f.close()
+    return ('', 204)
